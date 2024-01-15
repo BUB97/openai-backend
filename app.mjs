5ac2cd99f0c98be0cf3720ea9f22dbc978a3a202
BUB97 <shuhang_zhan@sina.com>
2024-01-16 00:28:06 +0800

    feat: 初始化

---
+++ app.mjs
@@ -0,0 +1,53 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+import OpenAI from 'openai';
+import http from 'http';
+import {HttpsProxyAgent} from 'https-proxy-agent';
+
+// 手动设置 CORS 头部
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // 允许所有来源
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    // 预检请求的响应
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+// 创建代理配置
+const proxyConfig = {
+  host: '127.0.0.1',
+  port: 7890,
+};
+
+// 创建代理
+const proxyAgent = new http.Agent(proxyConfig);
+
+const openai = new OpenAI({
+    apiKey: "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",
+    httpAgent: new HttpsProxyAgent("http://127.0.0.1:7890"),
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+  return completion.choices[0];
+}
+
+app.get('/', async (req, res) => {
+    const result = await main();
+    res.send(result);
+})
+
+app.listen(port, async () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
